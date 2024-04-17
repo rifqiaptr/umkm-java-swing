b2177cc85e1ca77580180a6e3b24f04f51e70e66
Isa <isaabqari@gmail.com>
2024-04-17 19:21:06 +0700

    ubah

---
+++ src/main/java/com/fluex404/penjualan/maven/PenjualanMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fluex404.penjualan.maven;
+
+/**
+ *
+ * @author isasubani
+ */
+public class PenjualanMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
