3a6733c2751e42e8d90430223d9e8838001291da
Isa <isaabqari@gmail.com>
2024-04-21 10:51:57 +0700

    nambahin file run untuk running

---
+++ src/main/java/com/fluex404/penjualan/maven/Run.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fluex404.penjualan.maven;
+
+import com.fluex404.penjualan.maven.Config.Koneksi;
+import com.fluex404.penjualan.maven.Main.FormLogin;
+import java.sql.Connection;
+
+/**
+ *
+ * @author isasubani
+ */
+public class Run {
+    public static void main(String[] args) {
+        System.out.println("Hallo World!!!");
+        
+        new FormLogin().show();
+    }
+}
